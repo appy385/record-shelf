2f563cab303a10614c05586d5e5a84bbcfc9be25
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 11:24:29 +0530

    feat(App): add route to sync page , add snapshot test , convert .css to .scss

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 describe('', () => {
-  test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+  test('shoudl match snapshot', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
   });
 });
