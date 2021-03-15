5d70ba0d099bca20608316e5532e67730bf95513
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 18:00:13 +0530

    feat(App): add Header component to all routes

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Sync from '../pages/Sync/Sync';
 import Music from '../pages/Music/Music';
+import Header from '../components/Header/Header';
 import './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <div className="app-container">
+      <Header />
       <Switch>
         <Route path="/songs"><Music /></Route>
         <Route path="/"><Sync /></Route>
