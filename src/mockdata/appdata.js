abeaf411b40d7d4ce31f7b899c0464df90bc16f2
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 15:53:47 +0530

    feat(mockdata): add mock data for helper function `groupByGenre` - mockSongsArray and mockResultSongByCatgeory

--- src/mockdata/appdata.js
+++ src/mockdata/appdata.js
@@ -7,4 +7,67 @@ export const mockSongData = {
   publishedAt: '2013-01-01T00:00:00',
 };
 
-export const newS = {};
+export const mockSongsArray = [
+  {
+    albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273d9009cc16eddbc6664508cd8',
+    artist: { id: 'a9d57d30-182f-4297-b5d9-f1e765c86935', name: 'Phillip Phillips' },
+    genre: { id: '8f63f8b8-a803-45da-8b2d-26a32ede9230', name: 'Country' },
+    id: '08d7e81e-1c34-4a60-a70c-2ec11c8a6687',
+    name: 'Gone, Gone, Gone',
+    publishedAt: '2013-01-01T00:00:00',
+  },
+  {
+    albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273d9009cc16eddbc6664508cd8',
+    artist: { id: 'a9d57d30-182f-4297-b5d9-f1e765c86935', name: 'Phillip Phillips' },
+    genre: { id: '8f63f8b8-a803-45da-8b2d-26a32ede9230', name: 'Country' },
+    id: '08d7e81e-1c34-4a60-a70c-2ec11c8a6687',
+    name: 'Gone, Gone, Gone',
+    publishedAt: '2013-01-01T00:00:00',
+  },
+
+];
+
+export const mockResultSongByCatgeory = {
+  Country: [
+    {
+      albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273d9009cc16eddbc6664508cd8',
+      artist: { id: 'a9d57d30-182f-4297-b5d9-f1e765c86935', name: 'Phillip Phillips' },
+      genre: { id: '8f63f8b8-a803-45da-8b2d-26a32ede9230', name: 'Country' },
+      id: '08d7e81e-1c34-4a60-a70c-2ec11c8a6687',
+      name: 'Gone, Gone, Gone',
+      publishedAt: '2013-01-01T00:00:00',
+    },
+    {
+      albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273d9009cc16eddbc6664508cd8',
+      artist: { id: 'a9d57d30-182f-4297-b5d9-f1e765c86935', name: 'Phillip Phillips' },
+      genre: { id: '8f63f8b8-a803-45da-8b2d-26a32ede9230', name: 'Country' },
+      id: '08d7e81e-1c34-4a60-a70c-2ec11c8a6687',
+      name: 'Gone, Gone, Gone',
+      publishedAt: '2013-01-01T00:00:00',
+    },
+  ],
+};
+
+export const mockSongsByCategory = {
+  Country: [
+    {
+      albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273d9009cc16eddbc6664508cd8',
+      artist: { id: 'a9d57d30-182f-4297-b5d9-f1e765c86935', name: 'Phillip Phillips' },
+      genre: { id: '8f63f8b8-a803-45da-8b2d-26a32ede9230', name: 'Country' },
+      id: '08d7e81e-1c34-4a60-a70c-2ec11c8a6687',
+      name: 'Gone, Gone, Gone',
+      publishedAt: '2013-01-01T00:00:00',
+    },
+  ],
+  Bollywoord: [
+    {
+      albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273b9c46d2b8d5604c33cc578b7',
+      artist: { id: 'a5185b4a-60de-456e-903f-501b6d8889ec', name: 'Jasmine Sandlas' },
+      genre: { id: '37146752-2c95-4204-a845-a32f3cdce9c8', name: 'Bollywood' },
+      id: '0a150e00-ef4e-4304-9a07-6a66892bfc7c',
+      name: 'Illegal Weapon 2.0 (From Street Dancer 3D)',
+      publishedAt: '2020-01-04T00:00:00',
+    },
+  ],
+
+};
