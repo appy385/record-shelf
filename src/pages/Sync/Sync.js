8d740b82c9334aae5418507e88b4949018fe052b
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 11:48:41 +0530

    feat(Sync): wrap sync button with link to /songs , update snapshots and tests to verify it goes to `/songs` route

--- src/pages/Sync/Sync.js
+++ src/pages/Sync/Sync.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Sync.scss';
 
 const Sync = () => (
   <div className="sync-container">
     <span>:((</span>
     <span>Seems a bit empty in here...</span>
-    <button type="button" className="sync-button">Sync</button>
+    <Link to="/songs"><button type="button" className="sync-button">Sync</button></Link>
   </div>
 );
 
--- src/pages/Sync/Sync.test.js
+++ src/pages/Sync/Sync.test.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Sync from './Sync';
 
 describe(Sync.name, () => {
   test('should match snapshot', () => {
-    const { container } = render(<Sync />);
+    const { container } = render(<BrowserRouter><Sync /></BrowserRouter>);
     expect(container).toMatchSnapshot();
   });
   test('should have message ', () => {
-    render(<Sync />);
+    render(<BrowserRouter><Sync /></BrowserRouter>);
     screen.getByText('Seems a bit empty in here...');
     screen.getByText(':((');
   });
   test('should have button sync', () => {
-    render(<Sync />);
+    render(<BrowserRouter><Sync /></BrowserRouter>);
     const syncButton = screen.getByText('Sync');
     expect(syncButton.tagName).toBe('BUTTON');
+  });
+  test('should go to route /songs', () => {
+    render(<BrowserRouter><Sync /></BrowserRouter>);
+    const syncButton = screen.getByText('Sync');
+    fireEvent.click(syncButton);
+    expect(document.location.href).toBe('http://localhost/songs');
   });
 });
