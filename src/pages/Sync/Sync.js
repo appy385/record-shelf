bf2e0c12995572ba7803d1b9ba6aaf56ba640382
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 18:20:32 +0530

    feat(Sync): add css to Sync button and text

--- src/pages/Sync/Sync.js
+++ src/pages/Sync/Sync.js
@@ -4,8 +4,8 @@ import './Sync.scss';
 
 const Sync = () => (
   <div className="sync-container">
-    <span>:((</span>
-    <span>Seems a bit empty in here...</span>
+    <span className="sync-text">:((</span>
+    <span className="sync-text">Seems a bit empty in here...</span>
     <Link to="/songs"><button type="button" className="sync-button">Sync</button></Link>
   </div>
 );
