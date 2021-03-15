0743769ac33f3fc656d310a8a19b095cd68bfbec
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 11:47:28 +0530

    feat(App): add route /music for Music page , add tests for sync page button and its click properties

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Sync from '../pages/Sync/Sync';
+import Music from '../pages/Music/Music';
 import './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <div className="app-container">
       <Switch>
+        <Route path="/songs"><Music /></Route>
         <Route path="/"><Sync /></Route>
       </Switch>
     </div>
--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-describe('', () => {
+describe(App.name, () => {
   test('shoudl match snapshot', () => {
     const { container } = render(<App />);
     expect(container).toMatchSnapshot();
+  });
+  test('should render button sync', () => {
+    render(<App />);
+    const syncButton = screen.getByText('Sync');
+    expect(syncButton.tagName).toBe('BUTTON');
+  });
+  test('should go to /songs route on sync button click ', () => {
+    render(<App />);
+    const syncButton = screen.getByText('Sync');
+    fireEvent.click(syncButton);
+    expect(document.location.href).toBe('http://localhost/songs');
+    screen.getByText('Music');
   });
 });
