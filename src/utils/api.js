00e0ba640015814bfdbb1cd31e7283a255b52765
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 17:01:24 +0530

    feat(utils): add two api calls  getLikes and updateLike

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,4 +6,19 @@ export const getSongs = async () => {
   return res.data;
 };
 
+export const getLikes = async (id) => {
+  const opts = { headers: { Authorization: `Bearer ${process.env.REACT_APP_TOKEN}` } };
+  const res = await axios.get(`/api/records/${id}/likes`, opts);
+  return res.data;
+};
+
+export const updateLike = async (id, value) => {
+  const body = {
+    like: value,
+  };
+  const opts = { headers: { Authorization: `Bearer ${process.env.REACT_APP_TOKEN}` } };
+  const res = await axios.patch(`/api/records/${id}/likes`, body, opts);
+  return res.data;
+};
+
 // export default getSongs;
