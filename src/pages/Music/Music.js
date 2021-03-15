29d0d19956cae04b1904b49651aa25affd0d2d65
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 12:42:14 +0530

    feat(Music): call getSongs api call and renders songs and  update snapshots

--- src/pages/Music/Music.js
+++ src/pages/Music/Music.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getSongs from '../../utils/api';
 import './Music.scss';
 
-const Music = () => (
-  <div className="music-container">
-    Music
-  </div>
-);
+const Music = () => {
+  const [songs, setSongs] = useState([]);
+  useEffect(async () => {
+    const songList = await getSongs();
+    console.log(songList.data);
+    setSongs(songList.data);
+  }, []);
+  return (
+    <div className="music-container">
+      {songs.map((song) => (song.id))}
+    </div>
+  );
+};
 
 export default Music;
