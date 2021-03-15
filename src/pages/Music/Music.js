a8851bee16494206a3d3357e6f7e60a4b0432a3b
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 15:56:23 +0530

    feat(Music): add toggle between genre and all songs  , use Genre component tto render songs by genre, update css and snapshot

--- src/pages/Music/Music.js
+++ src/pages/Music/Music.js
@@ -1,20 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { getSongs } from '../../utils/api';
 import Song from '../../components/Song/Song';
+import iconGenre from '../../assets/icon-genre.svg';
+import iconGrid from '../../assets/icon-grid.svg';
 import './Music.scss';
+import groupByGenre from '../../utils/helper';
+import Genre from '../../components/Genre/Genre';
 
 const Music = () => {
   const [songs, setSongs] = useState([]);
+  const [genreSongs, setGenreSongs] = useState({});
+  const [isGenre, setIsGenre] = useState(false);
   useEffect(async () => {
     const songList = await getSongs();
+    const newGenreSongs = groupByGenre(songList.data);
+    setGenreSongs(newGenreSongs);
     setSongs(songList.data);
   }, []);
+  const toggle = () => {
+    setIsGenre(!isGenre);
+  };
   return (
     <div className="music">
-      <span className="music-allsongs">All Songs</span>
-      <div className="music-container">
-        {songs.map((song) => (<Song key={song.id} song={song} />))}
-      </div>
+      {(isGenre) ? (
+        <>
+          <div className="music-header-container">
+            <span className="music-header-message">Genre</span>
+            <input type="image" className="music-toggle-button" onClick={toggle} src={iconGrid} alt="text" />
+          </div>
+          <div className="music-container">
+            <Genre genreSongs={genreSongs} />
+          </div>
+        </>
+      )
+        : (
+          <>
+            <div className="music-header-container">
+              <span className="music-header-message">All Songs</span>
+              <input type="image" className="music-toggle-button" onClick={toggle} src={iconGenre} alt="text" />
+            </div>
+            <div className="music-container">
+              {songs.map((song) => (<Song key={song.id} song={song} />))}
+            </div>
+          </>
+        )}
+
     </div>
   );
 };
