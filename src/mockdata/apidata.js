c0e16ad75a39fe0627d080388003fdb6379a797c
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-15 13:56:25 +0530

    feat(mockdata): add mockdata for getSongs api call

---
+++ src/mockdata/apidata.js
@@ -0,0 +1,26 @@
+export const mockgetSongsApi = {
+  data: {
+    data: [
+      {
+        albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273dc1f496d2a8d75f7fda092b6',
+        artist: { id: 'b22d7ed5-3ded-4958-a036-60453b7fc102', name: 'Neha Kakkar' },
+        genre: { id: '37146752-2c95-4204-a845-a32f3cdce9c8', name: 'Bollywood' },
+        id: '07075cdb-9e5c-41ec-8c09-9a1322dd92d8',
+        name: 'Aankh Marey (From Simmba)',
+        publishedAt: '2018-12-06T00:00:00',
+      },
+      {
+        albumArtUrl: 'https://i.scdn.co/image/ab67616d0000b273d9009cc16eddbc6664508cd8',
+        artist: { id: 'a9d57d30-182f-4297-b5d9-f1e765c86935', name: 'Phillip Phillips' },
+        genre: { id: '8f63f8b8-a803-45da-8b2d-26a32ede9230', name: 'Country' },
+        id: '08d7e81e-1c34-4a60-a70c-2ec11c8a6687',
+        name: 'Gone, Gone, Gone',
+        publishedAt: '2013-01-01T00:00:00',
+      },
+
+    ],
+  },
+
+};
+
+export const newData = {};
